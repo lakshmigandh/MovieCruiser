6ecba6aba8625f2e1689c826ff5a4f6818cf09c2
LakshmiGandh.M <lakshmigandh.m@cognizant.com>
2018-03-30 15:41:33 +0000

    First Commit

---
+++ src/main/java/com/cts/sr/moviecruiser/MoviecruiserApplication.java
@@ -0,0 +1,12 @@
+package com.cts.sr.moviecruiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecruiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecruiserApplication.class, args);
+	}
+}
