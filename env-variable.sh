fce52b851c302561599ac1feea1053c7bcbe2a86
lakshmigandh <lakshmigandh@gmail.com>
2018-05-02 18:28:57 +0530

    gitlab changes

--- env-variable.sh
+++ env-variable.sh
@@ -1,4 +1,4 @@
-export MYSQL_DATABASE=moviedb
-export MYSQL_CI_URL=jdbc:mysql://localhost:3306/moviedb
-export MYSQL_USER=root
-export MYSQL_PASSWORD=root
+export MYSQL_DATABASE=movieDb
+export MYSQL_CI_URL=jdbc:mysql://localhost:3306/movieDb
+export MYSQL_USER=app-root
+export MYSQL_PASSWORD=root123
